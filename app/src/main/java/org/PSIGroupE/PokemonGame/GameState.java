21a921df865b599f939ae9bcfc06f841d60b88ea
Robert Sonnenburg <robertsonnenburg@MacBook-Pro-von-Robert.local>
2024-12-05 15:00:43 +0100

    Turned PokemonGame into a state machine to be more functional with the GUI

---
+++ app/src/main/java/org/PSIGroupE/PokemonGame/GameState.java
@@ -0,0 +1,3 @@
+package org.PSIGroupE.PokemonGame;
+
+public enum GameState {S0AwaitingTeamSelection, S1AwaitingAction, S2AwaitingNextPokemon, S3GameOver}
--- app/src/main/java/org/PSIGroupE/PokemonGame/PokemonGame.java
+++ app/src/main/java/org/PSIGroupE/PokemonGame/PokemonGame.java
@@ -1,118 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.PSIGroupE.PokemonGame;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
-
-/**
- *
- * @author Robert Sonnenburg
- */
 public class PokemonGame {
 
+    private GameState gameState;
     private Player player1;
     private Player player2;
     private List<String> pokemonList;
-    private final Scanner scanner;
     private final Random random;
-    private boolean gameOver;
     private Player winner;
-    
+
     public PokemonGame() {
-        scanner = new Scanner(System.in);
         random = new Random();
         createPokemonList();
-        gameOver = false;
+        gameState = GameState.S0AwaitingTeamSelection;
     }
 
-    public PokemonGame(int doesntmatter) {
-        Pokemon[] pokemonArray1 = new Pokemon[4];
-        Pokemon[] pokemonArray2 = new Pokemon[4];
-        pokemonArray1[0] = PokemonFactory.createPokemon("Blastoise");
-        pokemonArray1[1] = PokemonFactory.createPokemon("Flareon");
-        pokemonArray1[2] = PokemonFactory.createPokemon("Garganacl");
-        pokemonArray1[3] = PokemonFactory.createPokemon("Gigalith");
-        pokemonArray2[0] = PokemonFactory.createPokemon("Hippowdon");
-        pokemonArray2[1] = PokemonFactory.createPokemon("Leafeon");
-        pokemonArray2[2] = PokemonFactory.createPokemon("Sandaconda");
-        pokemonArray2[3] = PokemonFactory.createPokemon("Sceptile");
-        player1 = new Player(1, pokemonArray1);
-        player2 = new Player(2, pokemonArray2);
-    }
+    public GameState startGame(String[] selectedPokemon) {
+        Pokemon[] team1 = createTeam(selectedPokemon);
+        Pokemon[] team2 = createRandomTeam();
 
-    private void start() {
-        Pokemon[] team1 = selectTeam();
-        Pokemon[] team2 = selectRandomTeam();
-        
         player1 = new Player(1, team1);
         player2 = new Player(2, team2);
 
-        while(!gameOver) {
-            playRound();
-        }
-
-        if (winner == player1) {
-            System.out.println("You won!");
-        } else {
-            System.out.println("You lost!");
-        }
+        gameState = GameState.S1AwaitingAction;
+        return gameState;
     }
 
-    private Pokemon[] selectTeam() {
-        Pokemon[] team = new Pokemon[4];
-        boolean selectionValid = false;
-        while (selectionValid == false) {
-            System.out.println("Please select 4 Pokemon (e.g. 1,2,4,7):");
-            for (int i = 0; i < pokemonList.size(); i++) {
-                System.out.println( (i+1) + ". " + pokemonList.get(i));
-            }
-
-            String selection = scanner.next();
-            String[] selectedPokemonStr = selection.split(",");
-            if (selectedPokemonStr.length != 4) {
-                System.out.println("Please enter exactly 4 integers separated by commas.");
-                continue;
-            }
-            
-            int[] selectedPokemonInt = new int[4];
-
-            try {
-                for (int i = 0 ; i < 4; i++) {
-                    selectedPokemonInt[i] = Integer.parseInt(selectedPokemonStr[i]);
-                    if (selectedPokemonInt[i] < 1 || selectedPokemonInt[i] > 10) {
-                        throw new IllegalArgumentException("Selection not in range");
-                    }
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please ensure all entries are integers.");
-                continue;
-            } catch (IllegalArgumentException e) {
-                System.out.println("Invalid input. Selection not in range.");
-                continue;
-            }
-
-            selectionValid = true;
-
-            for (int i = 0; i < 4; i++) {
-                String selectedName = pokemonList.get(selectedPokemonInt[i] - 1);
-                team[i] = PokemonFactory.createPokemon(selectedName);
-            }
+    private Pokemon[] createTeam(String[] selectedPokemon) {
+        if (selectedPokemon.length != 4) {
+            throw new IllegalArgumentException("There must be 4 selected Pokemon!");
         }
+
+        Pokemon[] team = new Pokemon[4];
+        
+        for (int i = 0; i < 4; i++) {
+            String pokemonName = selectedPokemon[0];
+            if (pokemonName == null) {
+                throw new IllegalArgumentException("The selected Pokemon cannot be null!");
+            }
+            team[i] = PokemonFactory.createPokemon(pokemonName);
+        }
+
         return team;
     }
 
-    private Pokemon[] selectRandomTeam() {
+    private Pokemon[] createRandomTeam() {
         Pokemon[] randomTeam = new Pokemon[4];
+        List<String> pool = new ArrayList<>(pokemonList);
         for (int i = 0; i < 4; i++) {
-            int index = random.nextInt(10);
+            int index = random.nextInt(pool.size());
             String selectedName = pokemonList.get(index);
             randomTeam[i] = PokemonFactory.createPokemon(selectedName);
+            pool.remove(index);
         }
         return randomTeam;
     }
@@ -131,79 +74,17 @@ public class PokemonGame {
         pokemonList.add("Vaporeon");
     }
 
-    private void playRound() {
-        int move1 = selectMove();
-        int move2 = selectRandomMove();
-        playMoves(move1, move2);
-        if (!player1.canPlay()) {
-            gameOver = true;
-            winner = player2;
-        } else if (!player2.canPlay()) {
-            gameOver = true;
-            winner = player1;
-        }
-    }
-
-    private int selectMove() {
-        boolean inputValid = false;
-        int selection = 0;
-        while (!inputValid) {
-            System.out.println();
-            System.out.println("Opponent Pokemon: " + player2.getActivePokemon().getName());
-            System.out.println("HP: " + player2.getActivePokemon().getHp());
-            System.out.println("Your Pokemon: " + player1.getActivePokemon().getName());
-            System.out.println("HP: " + player1.getActivePokemon().getHp());
-            System.out.println();
-            System.out.println("What do you want to do?");
-            Attack[] attacks1 = player1.getActivePokemon().getAttacks();
-            System.out.println("1. " + attacks1[0].getName());
-            System.out.println("2. " + attacks1[1].getName());
-            System.out.println("3. " + attacks1[2].getName());
-            System.out.println("4. " + attacks1[3].getName());
-            System.out.println("5. Switch Pokemon");
-            try {
-                selection = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Please enter an integer");
-                continue;
+    public GameState playAction(int action) {
+        if (action > 4) {
+            if (action / 10 != 5 || action % 10 > 3) {
+                throw new IllegalArgumentException("Invalid action");
             }
-            if (selection < 1 || selection > 5) {
-                System.out.println("Invalid Selection");
-                continue;
-            }
-            inputValid = true;
+        } else if (action < 1) {
+            throw new IllegalArgumentException("Invalid action");
         }
 
-        if (selection == 5) {
-            int pokemonToSwitchTo = selectSwitch();
-            selection = selection * 10 + pokemonToSwitchTo;
-        }
-
-        return selection;
-    }
-
-    private int selectSwitch() {
-        int selection = 0;
-        boolean inputValid = false;
-        while (!inputValid) {
-            System.out.println("Select Pokemon to switch to:");
-            System.out.println("1. " + player1.getTeam()[0].getName()); // TODO: show only conscious Pokemon
-            System.out.println("2. " + player1.getTeam()[1].getName());
-            System.out.println("3. " + player1.getTeam()[2].getName());
-            System.out.println("4. " + player1.getTeam()[3].getName());
-            try {
-                selection = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Please enter an integer");
-                continue;
-            }
-            if (selection < 1 || selection > 4) {
-                System.out.println("Invalid Selection");
-                continue;
-            }
-            inputValid = true;
-        }
-        return selection - 1;
+        playMoves(action, selectRandomMove());
+        return gameState;
     }
 
     private int selectRandomMove() {
@@ -234,67 +115,93 @@ public class PokemonGame {
 
         if (fasterMove > 4) {
             fasterPlayer.setActivePokemon(fasterMove % 10);
-            System.out.println("Player " + fasterPlayer.getId() + " switched to " + fasterPlayer.getActivePokemon().getName());
+            // Call a GUI function to inform about Pokemon switch of faster player
         }
 
         if (slowerMove > 4) {
             slowerPlayer.setActivePokemon(slowerMove % 10);
-            System.out.println("Player " + slowerPlayer.getId() + " switched to " + slowerPlayer.getActivePokemon().getName());
+            // Call a GUI function to inform about Pokemon switch of slower player
         }
 
         if (fasterMove <= 4) {
-            int damage = fasterPlayer.getActivePokemon().getAttacks()[fasterMove - 1].getPower() / 4; // Very simple
+            int damage = computeDamage(fasterPlayer.getActivePokemon().getAttacks()[fasterMove - 1], slowerPlayer.getActivePokemon().getType()); // Very simple
             slowerPlayer.getActivePokemon().decreaseHp(damage);
-            System.out.println(fasterPlayer.getActivePokemon().getName() + " used "
-                        + fasterPlayer.getActivePokemon().getAttacks()[fasterMove - 1].getName());
-            System.out.println(slowerPlayer.getActivePokemon().getName() + " lost " + damage + " HP.");
+            // Call GUI function to inform about attack used and damage
         }
 
         if (!slowerPlayer.getActivePokemon().isConscious()) {
             slowerPlayer.faint();
-            System.out.println(slowerPlayer.getActivePokemon().getName() + " fainted.");
+            // Call GUI function to inform about faint
+
             if (slowerPlayer.canPlay()) {
-                //Sorry  for the following abomination haha
-                // Basically, it selects a random conscious Pokemon as the new active Pokemon
-                slowerPlayer.setActivePokemon(Integer.parseInt(slowerPlayer.getConsciousPokemon().get(random.nextInt(slowerPlayer.getConsciousPokemon().size()))));
-                System.out.println("Player " + slowerPlayer.getId() + " put " + slowerPlayer.getActivePokemon().getName() + " into battle.");
+                if (slowerPlayer == player2) {
+                    //Select random next Pokemon if it is the bot
+                    slowerPlayer.setActivePokemon(Integer.parseInt(slowerPlayer.getConsciousPokemon().get(random.nextInt(slowerPlayer.getConsciousPokemon().size()))));
+                    // Call GUI method to inform about new Pokemon
+                } else {
+                    gameState = GameState.S2AwaitingNextPokemon;
+                }
+                
             } else {
-                gameOver = true;
                 winner = fasterPlayer;
-                return;
+                gameState = GameState.S3GameOver;
             }
 
         } else if (slowerMove <= 4) {
             int damage = slowerPlayer.getActivePokemon().getAttacks()[slowerMove - 1].getPower() / 4; // Very simple
             fasterPlayer.getActivePokemon().decreaseHp(damage);
-            System.out.println(slowerPlayer.getActivePokemon().getName() + " used "
-                        + slowerPlayer.getActivePokemon().getAttacks()[slowerMove - 1].getName());
-            System.out.println(fasterPlayer.getActivePokemon().getName() + " lost " + damage + " HP.");
+            // Call GUI function to inform about attack used and damage
         }
 
         if (!fasterPlayer.getActivePokemon().isConscious()) {
             fasterPlayer.faint();
-            System.out.println(fasterPlayer.getActivePokemon().getName() + " fainted.");
+            // Call GUI function to inform about faint
+
             if (fasterPlayer.canPlay()) {
-                fasterPlayer.setActivePokemon(Integer.parseInt(fasterPlayer.getConsciousPokemon().get(random.nextInt(fasterPlayer.getConsciousPokemon().size()))));
-                System.out.println("Player " + fasterPlayer.getId() + " put " + fasterPlayer.getActivePokemon().getName() + " into battle.");
+                if (fasterPlayer == player2) {
+                    fasterPlayer.setActivePokemon(Integer.parseInt(fasterPlayer.getConsciousPokemon().get(random.nextInt(fasterPlayer.getConsciousPokemon().size()))));
+                    // Call GUI method to inform about new Pokemon
+                } else {
+                    gameState = GameState.S2AwaitingNextPokemon;
+                }
             } else {
-                gameOver = true;
                 winner = slowerPlayer;
+                gameState = GameState.S3GameOver;
             }
         }
     }
 
-		public Player getPlayer1() {
-			return player1;
-		}
+    private int computeDamage(Attack attack, Type pType) {
+        // not implemented yet
+    }
 
-		public Player getPlayer2() {
-			return player2;
-		}
+    public GameState selectNextPokemon(int next) {
+        if (next < 0 || next > 3) {
+            throw new IllegalArgumentException("Invalid next Pokemon index");
+        }
 
-    /* Uncommented Main for testing purposes
-    public static void main(String[] args) {
-        new PokemonGame().start();
-    } */
+        if(!player1.getConsciousPokemon().contains(String.valueOf(next))) {
+            throw new IllegalArgumentException("Chosen next pokemon is unconscious");
+        }
+
+        player1.setActivePokemon(next);
+        gameState = GameState.S1AwaitingAction;
+        return gameState;
+    }
+
+    public List<String> getPokemonList() {
+        return pokemonList;
+    }
+
+    public Player getPlayer1() {
+		return player1;
+	}
+
+	public Player getPlayer2() {
+		return player2;
+	}
+
+    public Player getWinner() {
+        return winner;
+    }
 }
